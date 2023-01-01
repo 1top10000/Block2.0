126cf7d891d7fe4e76eb48cea64d8855fd5fa9fa
1top10000 <107670523+1top10000@users.noreply.github.com>
2023-01-01 20:10:32 +0900

    Update block.js

--- block.js
+++ block.js
@@ -3,6 +3,23 @@ const Blockcolor = '#0079c0';  //블록색깔
 const Blockcolor2 = '#003655'; //어두운 블록색깔
 const getcolor = '#373737';
 
+// 클립보드 복사 함수 (ExpressBlock)
+function copy(val) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = val;
+    dummy.select();
+    try {
+        document.execCommand("copy");
+    }
+    catch {
+        alert('복사하기를 지원하지 않습니다.');
+    }
+    finally {
+        document.body.removeChild(dummy);
+    }
+}
+
 const blocks = [    // 블록 만들기
     {
         name: 'text1',
@@ -54,6 +71,56 @@ const blocks = [    // 블록 만들기
             const leftValue = script.getNumberValue("LEFTHAND", script);
             const rightValue = script.getNumberValue("RIGHTHAND", script);
             return Math.pow(leftValue, rightValue);
+        }
+    },
+    {
+        name: 'calc_rad',
+        template: '%1 도를 라디안으로 변환',
+        skeleton: "basic_string_field",
+        color: {
+            default: Blockcolor, //RGB 색깔
+            darken: Blockcolor2, //RGB 색깔
+        },
+        params: [
+            {
+                type: 'Block',
+                accept: 'string',
+                value: '229.183'
+            },
+        ],
+        def: [],
+        map: {
+            d: 0,
+        },
+        class: "Block2.0",
+        func: async (sprite, script) => {
+            const g = script.getNumberValue("d", script);
+            return Math.radians(g);
+        }
+    },
+    {
+        name: 'calc_deg',
+        template: '%1 라디안을 도로 변환',
+        skeleton: "basic_string_field",
+        color: {
+            default: Blockcolor, //RGB 색깔
+            darken: Blockcolor2, //RGB 색깔
+        },
+        params: [
+            {
+                type: 'Block',
+                accept: 'string',
+                value: '4'
+            },
+        ],
+        def: [],
+        map: {
+            d: 0,
+        },
+        class: "Block2.0",
+        func: async (sprite, script) => {
+            const g = script.getNumberValue("d", script);
+            return Math.degrees(g);
         }
     },
     {
@@ -195,6 +262,36 @@ const blocks = [    // 블록 만들기
         class: "Block2.0",
         func: async (sprite, script) => {
             open(script.getValue('V', script));
+            return script.callReturn();
+        }
+    },
+    {
+        name: 'copy',
+        template: '%1 복사하기%2',
+        skeleton: "basic",
+        color: {
+            default: Blockcolor, //RGB 색깔
+            darken: Blockcolor2, //RGB 색깔
+        },
+        params: [
+            {
+                type: 'Block',
+                accept: 'string',
+                value: '안녕',
+            },
+            {
+                type: 'Indicator',
+                img: 'block_icon/hardware_icon.svg',
+                size: 11,
+            }
+        ],
+        def: [],
+        map: {
+            V: 0
+        },
+        class: "Block2.0",
+        func: async (sprite, script) => {
+            copy(script.getValue('V', script));
             return script.callReturn();
         }
     },
