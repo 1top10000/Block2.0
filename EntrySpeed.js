ec0443efd202a851aab9460a877a900144c8f30b
1top10000 <107670523+1top10000@users.noreply.github.com>
2023-11-17 19:04:05 +0900

    Update EntrySpeed.js

--- EntrySpeed.js
+++ EntrySpeed.js
@@ -1,5 +1,10 @@
 let PTemp = Entry.exportProject();
-PTemp.speed = prompt("반복문 속도(60은 기본값)");
-Entry.clearProject();
-Entry.loadProject(PTemp);
-alert("1top10020이 만들음");
+const prompt01 = prompt("반복문 속도(60은 기본값)");
+if (isNaN(prompt01) == false) {
+  PTemp.speed = Number(prompt01);
+  Entry.clearProject();
+  Entry.loadProject(PTemp);
+  alert("1top10020이 만들음(약250보다 빨라지는거는 불가능했어요.(1top10020컴 기준))");
+}else{
+  alert("숫자를 입력해주세요");
+}
